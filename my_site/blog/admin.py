e391fb6642fd9a619abb16940d641df0802f19a0
homabakhtiarian <homabakhtyaryan@gmail.com>
2022-04-09 14:05:35 +0430

    Added and modified admin and models files

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author")
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "tags", "date")
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
